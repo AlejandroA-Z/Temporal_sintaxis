12939ab10910fcfbef038072a25872c559c9b43a
Alejandro Aguilar Zabala <aaguilarzabala@frba.utn.edu.ar>
2026-05-06 02:07:35 -0300

    Update String.h
    Propuesta de cambio

--- String.h
+++ String.h
@@ -2,3 +2,18 @@
 #include <ctype.h>
 
 int IsEmpty(const char *str);
+
+//Propuesta
+
+#ifndef STRINGS_H_INCLUDED
+#define STRINGS_H_INCLUDED
+
+int IsEmpty(const char *str);
+
+// Calcula la longitud de la cadena de forma recursiva
+int GetLength(const char *str);
+
+// Compara si dos cadenas son iguales
+int AreEqual(const char *s1, const char *s2);
+
+#endif
