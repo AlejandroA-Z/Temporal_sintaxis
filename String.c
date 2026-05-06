a5dd573e37c4fd9d5df53b900f0e63f805aec968
Alejandro Aguilar Zabala <aaguilarzabala@frba.utn.edu.ar>
2026-05-06 02:15:15 -0300

    Update String.c
    Propuesta de cambio

--- String.c
+++ String.c
@@ -15,3 +15,38 @@ int IsEmpty(const char *str)
     }
     return 0;
 }
+
+//Propuesta
+
+#include "Strings.h"
+
+int IsEmpty(const char *str) {
+    return str[0] == '\0';
+}
+
+ 
+//   Implementación Recursiva: 
+
+
+int GetLength(const char *str) {
+    if (str[0] == '\0') {
+        return 0;
+    }
+    return 1 + GetLength(str + 1);
+}
+
+ 
+ //  Implementación de AreEqual:
+//   Compara caracter por caracter
+
+int AreEqual(const char *s1, const char *s2) {
+    int i = 0;
+    while (s1[i] != '\0' && s2[i] != '\0') {
+        if (s1[i] != s2[i]) {
+            return 0;
+        }
+        i++;
+    }
+    // Son iguales solo si ambas terminaron al mismo tiempo
+    return s1[i] == s2[i];
+}
