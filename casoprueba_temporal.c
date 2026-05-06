5be44df4fc58ac0e13d91ee2e46b010a194f59c8
Alejandro Aguilar Zabala <aaguilarzabala@frba.utn.edu.ar>
2026-05-06 02:20:13 -0300

    Update casoprueba_temporal.c
    Propuesta de cambio

--- casoprueba_temporal.c
+++ casoprueba_temporal.c
@@ -16,3 +16,28 @@ int main()
 
     return EXIT_SUCCESS;
 }
+
+//Propuesta
+
+#include <assert.h>
+#include <stdlib.h>
+#include "Strings.h"
+
+int main(void) {
+    // Tests para IsEmpty
+    assert(IsEmpty("") == 1);
+    assert(IsEmpty("hola") == 0);
+
+    // Tests para GetLength
+    assert(GetLength("") == 0);
+    assert(GetLength("abc") == 3);
+    assert(GetLength("Hola Mundo") == 10);
+
+    // Tests para AreEqual
+    assert(AreEqual("utn", "utn") == 1);
+    assert(AreEqual("utn", "utnfrba") == 0);
+    assert(AreEqual("", "") == 1);
+    assert(AreEqual("ABC", "abc") == 0);
+
+    return EXIT_SUCCESS;
+}
